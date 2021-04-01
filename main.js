0865af4bdd2212c7daa46e6f320da0729b7d3ac7
Nicolas HODEE <accounts@nicolashodee.com>
2021-04-01 21:25:58 +0100

    working on the accordion

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("JS loaded")
+const questions = document.querySelectorAll(".question"); 
+
+questions.forEach(question => {
+  question.addEventListener("click", () => {
+    let answer = question.nextElementSibling; 
+    let questionIcon = questionIcon.nextElementSibling;  
+
+    answer.classList.toggle("active");
+    questionIcon.classList.toggle("active"); 
+  }); 
+  
+}); 
