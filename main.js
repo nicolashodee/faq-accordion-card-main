68d6d4aaba9926ee5cd9062f0e2a65cafb0ab8a9
Nicolas HODEE <accounts@nicolashodee.com>
2021-04-03 21:48:48 +0100

    jworking on the javascript

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 console.log("JS loaded")
 const questions = document.querySelectorAll(".question"); 
+const questionContainer = document.querySelectorAll(".question-container");
+
 
 questions.forEach(question => {
   question.addEventListener("click", () => {
-    let answer = question.nextElementSibling; 
-    let questionIcon = questionIcon.nextElementSibling;  
+    let questionIcon = question.nextElementSibling; 
+    let answer = questionIcon.nextElementSibling; 
+    
+   
 
     answer.classList.toggle("active");
     questionIcon.classList.toggle("active"); 
